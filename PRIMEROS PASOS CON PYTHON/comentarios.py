954f443b16c64c27eec013c6a2aaf52020d02d90
alanmartinc <alancabot.ac@gmail.com>
2020-08-25 10:49:20 -0300

    Se agrega ejercicio primeros pasos con python

---
+++ PRIMEROS PASOS CON PYTHON/comentarios.py
@@ -0,0 +1,9 @@
+# Esto me mostrara el texto Hola por la consola
+print("Hola")
+# Estas dos instrucciones muestran texto por pantalla
+print("Mundo")
+print("!!")
+
+"""
+Esto es un comentario multilinea
+"""
